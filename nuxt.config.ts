1ac5eff10b245bf959ae19f5e5cd72dc9a764939
Gábor Torma <gabor@torma.dev>
2024-09-17 14:41:00 +0200

    chore: upgrade all deps.

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,6 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  compatibilityDate: '2024-07-18',
+  compatibilityDate: '2024-09-17',
   future: {
     compatibilityVersion: 4,
   },
