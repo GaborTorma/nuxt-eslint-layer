9e954ea782644bf04805c9c2bd450f57933854f7
Gábor Torma <gabor@torma.dev>
2024-11-26 15:21:04 +0100

    chore: upgrade all deps.

--- .playground/nuxt.config.ts
+++ .playground/nuxt.config.ts
@@ -1,5 +1,15 @@
+import { fileURLToPath } from 'node:url'
+
 export default defineNuxtConfig({
   compatibilityDate: '2024-09-19',
 
   extends: ['..'],
+
+  eslint: {
+    config: {
+      // Final solution for https://github.com/nuxt/eslint/issues/501 issue
+      // Use the generated ESLint config for lint root project as well
+      rootDir: fileURLToPath(new URL('..', import.meta.url)),
+    },
+  },
 })
