10dc70f24eb2e2580b3d2168e1dfb4ea4f2e6cb5
Gábor Torma <gabor@torma.dev>
2025-05-11 11:16:33 +0200

    chore: upgrade all deps.

--- .playground/nuxt.config.ts
+++ .playground/nuxt.config.ts
@@ -9,6 +9,7 @@ export default defineNuxtConfig({
     config: {
       // Final solution for https://github.com/nuxt/eslint/issues/501 issue
       // Use the generated ESLint config for lint root project as well
+      // It it works well, you need to get the following error: 20:3  error  The template root requires exactly one element  vue/no-multiple-template-root
       rootDir: fileURLToPath(new URL('..', import.meta.url)),
     },
   },
