fa9010445f54170bb807b7bff8c76a595c585cfa
Gábor Torma <gabor@torma.dev>
2025-05-18 10:13:15 +0200

    chore: upgrade all deps.

--- .release-it.ts
+++ .release-it.ts
@@ -1,4 +1,10 @@
 import { defineReleaseItConfig } from '@gabortorma/mwm'
 import { name } from './package.json'
 
-export default defineReleaseItConfig('nuxt-layer', name.split('/').pop())
+export default defineReleaseItConfig('nuxt-layer', name.split('/').pop(), {
+  plugins: {
+    'release-it-pnpm': {
+      publishCommand: 'pnpm publish --access public --no-git-checks --tag $tag',
+    },
+  },
+})
